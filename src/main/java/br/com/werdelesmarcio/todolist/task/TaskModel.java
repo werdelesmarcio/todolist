cc8b3d56c130859177781d99080758e4f4f52373
Werdeles Soares <werdelesmarcio@hotmail.com>
2023-10-15 00:48:50 -0300

    Alterações

--- src/main/java/br/com/werdelesmarcio/todolist/task/TaskModel.java
+++ src/main/java/br/com/werdelesmarcio/todolist/task/TaskModel.java
@@ -31,4 +31,10 @@ public class TaskModel {
     @CreationTimestamp
     private LocalDateTime createdAt;
 
+    public void setTitle(String title) throws Exception {
+        if(title.length() > 50){
+            throw new Exception("O campo de título deve conter no máximo 50 caracteres.");
+        }
+    }
+
 }
